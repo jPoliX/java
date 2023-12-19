19ad491568952457b954863e8c0da69d01c69d53
jPoliX <150335994+jPoliX@users.noreply.github.com>
2023-12-19 13:43:55 +0100

    Update cottura-uovo.java

--- cottura-uovo.java
+++ cottura-uovo.java
@@ -1,19 +1,32 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("partenza"); //inizia il programma
-        for(int i=1;i<=10;i++){ //usiamo il for per definire un numero preciso di casi
-            attesa(); //usiamo il metodo creato sotto
-            switch (i){ //facciamo uno switch case in cui abbiamo le 3 fasi di cottura dell'uovo
-                case 1:System.out.println("uovo dopo 20 secondi: ");break; //primo caso
-                case 2:System.out.println("uovo dopo 40 secondi: ");break; //secondo caso
-                case 3:System.out.println("uovo dopo 1 minuto: ");break; //terzo caso
-                default:System.out.println("tempo non valido"); 
+        Scanner scanner=new Scanner(System.in);
+        String risposta;
+        System.out.println("partenza");
+        for(int i=1;i<=11;i++){
+            attesa();
+            switch (i){
+                case 1:System.out.println("uovo iniziale: ");break;
+                case 2:System.out.println("uovo a metà: ");break;
+                case 3:System.out.println("uovo finale: ");break;
+                default:System.out.println("tempo non valido");
+            }
+             System.out.println("hai ancora fame?");
+             risposta=scanner.next();
+        if(risposta.equals("si")){
+            switch (i){
+                case 1:System.out.println("uovo iniziale: ");break;
+                case 2:System.out.println("uovo a metà: ");break;
+                case 3:System.out.println("uovo finale: ");break;
+                default:System.out.println("tempo non valido");
             }
         }
+        }
     }
-    public static void attesa(){ //creaiamo il metodo "attesa" 
+    public static void attesa(){
         try {
-            Thread.sleep( 20000); //quantità di tempo che deve passare in ogni fase
+            Thread.sleep( 5000);
         }catch (InterruptedException e){
             e.printStackTrace();
         }
