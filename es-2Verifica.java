fee23524a5b91c8bc94e657243129a04c1fae86c
jPoliX <150335994+jPoliX@users.noreply.github.com>
2024-05-21 09:05:01 +0200

    Update es-2Verifica.java

--- es-2Verifica.java
+++ es-2Verifica.java
@@ -2,43 +2,56 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int quantiNumeri = 0, n = 0,contaPari=0,posizione;
+        int quantiNumeri = 0, n = 0,contatorePari=0,indice=0;
         do {
             System.out.println("inserisci la dimensione");
             quantiNumeri = sc.nextInt();
-        } while (quantiNumeri < 8 || quantiNumeri > 11);
+        } while (quantiNumeri < 8 || quantiNumeri > 11); //controllo se la dimensione è coerente
         int[] array = new int[quantiNumeri];
 
         System.out.println("inserisci un numero");
             for (int i = 0; i < array.length; i++) {
                 do {
                     n = sc.nextInt();
-                    array[i] = n;
-                }while(n<0||n>31);
+                    array[i] = n; //inserisco i numeri nell'array
+                }while(n<0||n>31); //controllo se i valori sono coerenti
             }
-            for(int i=0;i< array.length;i++){
-                System.out.println("Array: "+array[i]);
+            int[] indiciPari = new int[quantiNumeri]; // memorizza gli indici dei numeri pari
+            
+             for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                indiciPari[contatorePari] = i;
+                contatorePari++;
             }
+        }
+           int[] arrayPari = new int[contatorePari];
+        for (int i = 0; i < contatorePari; i++) {
+            arrayPari[i] = array[indiciPari[i]];
+        }
+        selectionSort(arrayPari);
+
+        for (int i = 0; i < contatorePari; i++) {
+            array[indiciPari[i]] = arrayPari[i];
+        }
+            
+            for(int i=0;i<array.length;i++){
+                System.out.println("Array ordinato: "+array[i]);
+            }
+            
     }
     public static void selectionSort(int[]array){
-        for (int i = 0; i < array.length - 1; i++) {
+      for(int i = 0; i < array.length-1; i++) {
             int minimo = i;
-            if(array[i]%2==0) {
-                for (int j = i + 1; j < array.length; j++)
-                {
-                    if(array[j]%2==0) {
-                        if (array[minimo] > array[j]) {
-                            minimo = j;
-                        }
-                    }
+            for(int j = i+1; j < array.length; j++) {
+                if(array[minimo]>array[j]) {
+                    minimo = j;
                 }
             }
-            if (minimo != i) {
+            if(minimo!=i) {
                 int k = array[minimo];
-                array[minimo] = array[i];
+                array[minimo]= array[i];
                 array[i] = k;
             }
         }
-    }
-
+}
 }
